9c4110d424d538e87a5733c739508457187ac9d7
Ivan Politov <ivan.hanter21@mail.ru>
2024-08-04 17:29:07 +0500

    Using PostgreSQL DB

--- ngp_help/ngp_help/settings.py
+++ ngp_help/ngp_help/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'ngp_help.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'name',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
